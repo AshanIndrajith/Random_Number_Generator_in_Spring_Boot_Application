5f6a6af31804418b736b4fae9faf88472d4d9696
Ashan Indrajith <103479897+AshanIndrajith@users.noreply.github.com>
2023-08-21 18:34:32 +0530

    updated

--- Random/Random/src/main/java/com/example/Random/Repo/StudentRepo.java
+++ Random/Random/src/main/java/com/example/Random/Repo/StudentRepo.java
@@ -2,6 +2,8 @@ package com.example.Random.Repo;
 
 import com.example.Random.Entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepo extends JpaRepository<Student,Integer> {
 }
